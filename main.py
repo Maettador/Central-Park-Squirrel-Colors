1ad702d9286c8e361b0a876b054ced8f8fd56223
Maettador <83495799+Maettador@users.noreply.github.com>
2021-08-31 21:50:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import csv
+import pandas as pd
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [2473, 392, 103]
+}
+
+df = pd.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
